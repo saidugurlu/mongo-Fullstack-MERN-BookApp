20c8373f019c4e2d5bdadb1b73f6e84094de5a4d
saidugurlu <ugurlumsaid@gmx.de>
2022-07-05 13:58:37 +0200

    have all books on get

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,26 @@
-import './App.scss'
+import { useEffect, useState } from 'react';
+import './App.scss';
+import axios from 'axios';
+
+const url = 'http://localhost:3459';
 
 function App() {
-  return (
-    <div className="App">
-      <h1>Book Site</h1>
-    </div>
-  )
+	const [books, setBooks] = useState([]);
+
+	useEffect(() => {
+		(async () => {
+			const _books = (await axios.get(`${url}/book`)).data.books;
+			setBooks(_books);
+		})();
+	}, []);
+
+	return (
+		<div className="App">
+			<h1>Book Site</h1>
+
+			<p>There are {books.length} books.</p>
+		</div>
+	);
 }
-export default App
+
+export default App;
