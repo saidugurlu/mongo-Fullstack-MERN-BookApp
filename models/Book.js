8ffec69f2f1bb995a1cc92d3034cb167722e5d55
saidugurlu <ugurlumsaid@gmx.de>
2022-07-05 09:58:00 +0200

    test-book created inPostman, checked in M- Compass

--- models/Book.js
+++ models/Book.js
@@ -1,5 +1,5 @@
 import mongoose from 'mongoose';
- 
+
 const bookSchema = new mongoose.Schema({
     title: String,
     description: String,
--- server.js
+++ server.js
@@ -1,5 +1,7 @@
 import express from 'express';
 import mongoose from 'mongoose';
+import { Book } from './models/Book.js';
+
  
 mongoose.connect('mongodb://localhost/mongo-Fullstack-MERN-App-001');
  
@@ -8,6 +10,20 @@ const port = 3022;
  
 app.get('/', (req, res) => {
     res.send('<h1>Book Site API</h1>');
+});
+
+app.post('/book', async (req, res) => {
+    const book = new Book({
+        title: 'ttt',
+        description: 'ddd',
+        numberOfPages:999
+    });
+    await book.save();
+ 
+    console.log('book created: ' + new Date());
+    res.status(200).json({
+        message: 'book was created'
+    });
 });
  
 app.listen(port, () => {
