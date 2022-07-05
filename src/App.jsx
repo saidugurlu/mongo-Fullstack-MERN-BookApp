ee62462d31a38ffbf132e759efcaf3222681a500
saidugurlu <ugurlumsaid@gmx.de>
2022-07-05 14:01:47 +0200

    showing books as div panels

--- src/App.jsx
+++ src/App.jsx
@@ -19,6 +19,20 @@ function App() {
 			<h1>Book Site</h1>
 
 			<p>There are {books.length} books.</p>
+
+			<div className="books">
+				{books.map((book, i) => {
+					return (
+						<div className="book">
+              <img src={book.imageUrl}/>
+              <div className="info">
+							<div className="title">{book.title}</div>
+              <div className="description">{book.description}</div>
+              </div>
+						</div>
+					);
+				})}
+			</div>
 		</div>
 	);
 }
